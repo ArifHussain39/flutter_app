74960f858b8caf04866c8d02fa37864df7e3b2c2
Arif Hussain <arif3337179@gmail.com>
2024-04-19 14:24:22 +0500

    Firebase CRUD

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,116 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Center(
-    child: Text(
-      'Hello, World!',
-      textDirection: TextDirection.ltr,
+  runApp(MaterialApp(
+    title: "Flutter CRUD",
+    theme: ThemeData(
+      brightness: Brightness.dark,
+      primaryColor: Colors.purple,
+      hintColor: Colors.cyan,
     ),
+    home: MyApp(),
   ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String studentName='', studentID='', studyProgramID='';
+  double studentGPA=0.0;
+
+  getStudentName(name) {
+    this.studentName = name;
+  }
+  
+  getStudentID(id) {
+    this.studentID = id;
+  }
+
+  getStudyProgramID(programID){
+    this.studyProgramID = programID;
+  }
+
+  getStudentGPA(gpa){
+    this.studentGPA = double.parse(gpa);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter CRUD Operations'),
+        backgroundColor: Colors.purple,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    labelText: "Name",
+                    fillColor: Colors.white,
+                    focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Colors.blue, width: 2.0))),
+                onChanged: (String name) {
+                  getStudentName(name);
+                },
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    labelText: "Student ID",
+                    fillColor: Colors.white,
+                    focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Colors.blue, width: 2.0))),
+                onChanged: (String id) {
+                  getStudentID(id);
+                },
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    labelText: "Study Program ID",
+                    fillColor: Colors.white,
+                    focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Colors.blue, width: 2.0))),
+                onChanged: (String programID) {
+                  getStudyProgramID(programID);
+                },
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    labelText: "GPA",
+                    fillColor: Colors.white,
+                    focusedBorder: OutlineInputBorder(
+                        borderSide:
+                            BorderSide(color: Colors.blue, width: 2.0))),
+                onChanged: (String gpa) {
+                  getStudentGPA(gpa);
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 }
